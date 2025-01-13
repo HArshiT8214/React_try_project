5b83477a6c50480a0dc81658a60f9e8c9249e240
harshit singh <harshitsingh8214@gmail.com>
2025-01-13 20:33:32 +0530

    bhot din baad

---
+++ 12_Blog/src/appwrite/auth.js
@@ -0,0 +1,45 @@
+import conf from "../conf/conf.js"
+import { Client, Account, ID } from "appwrite"
+
+export class AuthService {
+    client = new Client()
+    account;
+    constructor() {
+        this.client.setEndpoint(conf.appwriteUrl).setProject(appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            console.log(userAccount);
+            if (userAccount) {
+                return this.login(email, password)
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("error in trying to get current user", error)
+        }
+        return null;
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
---
+++ 12_Blog/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Headers/Header.jsx";
+import footer from "./Footer/footer.jsx";
+
+export{
+    Header,
+    footer
+}
